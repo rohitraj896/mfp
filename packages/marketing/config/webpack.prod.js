888227ce03f5f5f53be02d8d2d6d1dcf5bedf37b
Rohit Kumar <rohitkr3390@gmail.com>
2024-07-06 14:27:57 +0530

    changes path

--- packages/marketing/config/webpack.prod.js
+++ packages/marketing/config/webpack.prod.js
@@ -11,7 +11,7 @@ const prodConfig = {
   mode: "production",
   output: {
     filename: "[name].[contenthash].js",
-    publicPath: "/marketing/latest",
+    publicPath: "/marketing/latest/",
   },
   plugins: [
     new ModuleFederationPlugin({
